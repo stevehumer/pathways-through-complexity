392be33eb3457e73736f524a639577ca646f825c
Stephen Humer <stevehumer@gmail.com>
2025-03-07 18:42:36 -0500

    attempt to fix header being larger on mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export const Header = () => {
     return (
       <header className="bg-bookYellow text-gray-600 body-font fixed top-0 w-full z-10 shadow-md">
-        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center justify-center">
+        <div className="container mx-auto flex p-3 md:p-5 flex-row items-center justify-center">
           <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
             <span className="header-font ml-3 text-xl">Pathways Through Complexity</span>
           </a>
