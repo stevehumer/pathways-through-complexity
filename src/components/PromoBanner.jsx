6fbb620c894617aaa187272b9ac55a17d4da8ef2
stevehumer <stevehumer@gmail.com>
2025-11-03 21:47:54 -0500

    content: promo update to school days

--- src/components/PromoBanner.jsx
+++ src/components/PromoBanner.jsx
@@ -22,9 +22,9 @@ export const PromoBanner = () => {
 
   return (
     <div className="bg-blue-100 text-blue-900 text-center py-3 px-4 shadow-md mt-14 md:mt-16">
-      📚 <strong>Free eBook!</strong> Download <em>“The Challenges at Azelica”</em> for free from <strong>Nov 14 - Nov 18, 2025</strong>. First book in the <em>Bine Ari Digit</em> fiction series! 👉{" "}
+      📚 <strong>Free eBook!</strong> Download <em>“School Days”</em> for free from <strong>Nov 14 - Nov 18, 2025</strong>. First book in the <em>Bine Ari Digit</em> fiction series! 👉{" "}
       <a
-        href="https://www.amazon.com/Challenges-Azelica-BAD-Adventures-Fable-ebook/dp/B0CP2QHQYC"
+        href="https://www.amazon.com/School-Days-Adventures-Fable-Digit-ebook/dp/B0DQ1V8P5V"
         onClick={handleFreeEbookClick}
         className="underline text-blue-800 font-semibold hover:text-blue-600"
       >
